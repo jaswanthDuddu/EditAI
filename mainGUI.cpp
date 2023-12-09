7f07d96f69903e7fcd73fbd41bc07962acb618e2
Sagar Sahu <145163371+sagars2004@users.noreply.github.com>
2023-12-08 19:21:16 -0500

    mainGUI.cpp
    Main file to create instance of MainWindow class, display the main window, and enter application's event loop.

---
+++ mainGUI.cpp
@@ -0,0 +1,10 @@
+// mainGUI.cpp
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
